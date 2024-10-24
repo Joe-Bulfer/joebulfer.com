bf667ea25aad87bc1e180d21d64ca17e9a4fab03
Joe Bulfer <jbulfer13@gmail.com>
2024-10-24 09:17:14 -0500

    giscus comment system

---
+++ docs/publish.js
@@ -0,0 +1,9 @@
+const hero_anchor = document.querySelector('a[title="Joe Bulfer"]');
+
+if (hero_anchor) {
+  hero_anchor.setAttribute('href', '/');
+}
+
+document.querySelector("footer").remove()
+
+
